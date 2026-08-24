ddea792ab7a1c6903d355589ad4ad52d5a2d999a
mehdi-taoufik <taoufikmehdi91@gmail.com>
2026-08-24 17:01:14 +0100

    fix: import Tauri Manager trait for production build

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents an extra console window on Windows in release builds.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{WebviewUrl, WebviewWindowBuilder};
+use tauri::{Manager, WebviewUrl, WebviewWindowBuilder};
 
 #[cfg(not(debug_assertions))]
 mod server {
